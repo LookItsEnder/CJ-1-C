53b1ab075e674c21406cbec625f3c60d9996a681
jevt04 <jared.e.vantassell@gmail.com>
2026-02-11 19:03:06 -0600

    Update Whateveryouwant.cpp
    started to make a fortune game that picks a color and number, I forgot how to do it but I'm not going to vibe code it because im not a loser

--- Whateveryouwant.cpp
+++ Whateveryouwant.cpp
@@ -1,26 +1,50 @@
 //
 // Created by cjf on 2/11/26.
-//
+// Edited by jevt04 on 2/11/26
 
 #include<bits/stdc++.h>
+#include <string>
+#include <vector> 
+#include <ctime>
 using namespace std;
 
 void fizzBuzz() {
     int num = 0;
     cout << "Enter a number: ";
     cin >> num;
-    if (num % 3 == 0 && num % 5 == 0) {
+    if (num % 3 == 0 && num % 5 == 0) 
+    {
         cout << "FizzBuzz" << endl;
     }
-    else if (num % 3 == 0) {
+    else if (num % 3 == 0) 
+    {
         cout << "Fizz" << endl;
     }
-    else if (num % 5 == 0) {
+    else if (num % 5 == 0) 
+    {
         cout << "Buzz" << endl;
     }
 }
 
+void Fortune()
+{
+    
+    string color;
+    int numeral;
+    cout << "Welcome to Fortune!! " << endl;
+    cout << "Pick a color (red, green, blue, yellow, purple";
+    cin >> color;
+    cout << "Now pick a number (1-10)";
+    cin >> numeral;
+    if (numeral < 1 || numeral > 10)
+    {
+        cout << "Invalid input!!";
+    }
+    if (color!=)
 
+
+
+}
 
 int main() {
     int choice = 0;
@@ -28,8 +52,14 @@ int main() {
     cout << "Hello Twin\nWelcome to my silly little game!\nWhat would you like to play?\nChoices are:\n1.FizzBuzz"<<endl;
     cout << ">";
     cin >> choice;
-    if (choice == 1) {fizzBuzz();}
-
+    if (choice == 1) 
+    {
+        fizzBuzz();
+    }
+    else if (choice == 2)
+    {
+        Fortune();
+    }
 
     return 0;
 }
